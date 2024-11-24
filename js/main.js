b675541c78a85a8c3fec8bebf2fc9a0b7d4ec74f
Youssef <youssimail@hotmail.com>
2024-11-24 21:57:31 +0100

    Creating modular functions + Fixing search algorithm's bug

--- js/main.js
+++ js/main.js
@@ -1,169 +1,182 @@
 //* Display "My lists" content when loading
 
 window.addEventListener("load", (event) => {
-  fillMyLists(event)
+  loadLists(event)
 })
-function fillMyLists(ev) {
-  if (myLists.length) {
-    //* != 0
+
+function loadLists(event) {
+  if (myLists.length > 0) {
     tasksContainer.classList.add("unhide")
-    for (let i = 0; i < myLists.length; i++) {
-      createList(myLists[i].id, myLists[i].listName, ev)
-    }
+    myLists.forEach(({ id, listName }) => {
+      createList(id, listName, event)
+    })
   }
 }
 
-//* Add New List and Save the entry in localStorage
-
+//* Add and save the new list and create its UI
 listsForm.addEventListener("submit", function (event) {
   event.preventDefault()
-  if (newEntry.value.trim()) {
+  const userInput = newEntry.value.trim()
+  if (userInput) {
     tasksContainer.classList.add("unhide")
-    let newId = saveList(newEntry.value) // Save the new list and get it's Id
-    createList(newId, newEntry.value, event)
+    let newListId = saveList(userInput) // Save the new list and get it's Id
+    createList(newListId, userInput, event)
     newEntry.value = ""
   }
 })
 
-function saveList(value) {
-  let obj = { id: Date.now(), listName: value, tasks: [] }
-  myLists.push(obj)
+function saveList(input) {
+  const newList = { id: Date.now(), listName: input, tasks: [] }
+  myLists.push(newList)
   storeUpdated()
-  return obj.id // To add To "data-list-id" Attribute of the new created Li
+  return newList.id // To add To "data-list-id" Attribute of the new created Li
 }
 
-function createList(listId, value, evt) {
+function createList(listId, value, event) {
   let newList = document.createElement("LI")
   newList.innerText = value
   newList.classList.add("list")
   newList.setAttribute("data-list-id", listId)
   myListsUl.appendChild(newList)
-  newList.addEventListener("click", (ev) => {
-    // the Function is executed after Clicking On the new created List (Here the targeted list is unknown)
-    listOnClick(ev.target, listId)
+  newList.addEventListener("click", () => {
+    handleListClick(newList, listId)
   })
 
-  // the 'listOnClick' function is executed when submiting new created List
-  // and it will active the Clicked-On list and display its tasks in the tasks window
-  if (evt.type == "submit") listOnClick(newList, listId)
+  // Trigger 'handleListClick' to display the new list in the tasks UI
+  if (event.type == "submit") handleListClick(newList, listId)
 }
 
 //* On List Click OR Submiting new created List
-
-function listOnClick(targetList, idList) {
+function handleListClick(targetList, listId) {
   let activeList = document.querySelector(".active-list") || false
   if (activeList) activeList.classList.remove("active-list") // if there is an active list
   targetList.classList.add("active-list")
   listName.innerText = targetList.innerText
-  listName.setAttribute("data-list-id", idList)
+  listName.setAttribute("data-list-id", listId)
 
-  fillMyTasks(idList)
+  loadTasks(listId)
 }
 
-//* Fill New List's Tasks
+//* Load New List's Tasks
+function loadTasks(listId) {
+  // Remove old list's tasks
+  myTasksUl.replaceChildren("")
 
-function fillMyTasks(idList) {
-  // Remove Old List's Tasks
-  while (myTasksUl.firstChild) {
-    // Exist
-    myTasksUl.lastChild.remove() // Faster then removing firstChild
-  } //OR we Can Simply Use: myTasksUl.textContent = '' // OR .replaceChildren('')
-
-  // Fill New List's Tasks
-  for (let i = 0; i < myLists.length; i++) {
-    if (myLists[i].id == idList) {
-      if (myLists[i].tasks.length) {
-        // if there is tasks
-        myLists[i].tasks.forEach((obj, index) => {
-          createTask(obj.taskContent, obj.complete, index)
-        })
-        refrechTasksCounter(myLists[i].tasks)
-      }
-    }
+  // Fill with new list's tasks
+  const list_found = findList(listId)
+  if (list_found && list_found.tasks.length > 0) {
+    list_found.tasks.forEach(createTask) // == .forEach(({ taskContent, complete }[= task], task_index) => {--code--})
+    refrechTasksCounter(list_found.tasks)
+  } else {
+    tasksCounter.innerText = "No tasks yet."
   }
 }
 
-function createTask(value, checkSt, index) {
-  // I used 'index' as a way to make id's unique
+// Find List by Id (Using Binary Search)
+function findList(listId) {
+  let start = 0
+  let end = myLists.length - 1
+  while (start <= end) {
+    const mid_index = Math.floor((start + end) / 2)
+    if (myLists[mid_index].id === listId) return myLists[mid_index]
+    if (myLists[mid_index].id < listId) start = mid_index + 1
+    if (myLists[mid_index].id > listId) end = mid_index - 1
+  }
+  return false
+}
+
+function createTask({ taskContent, complete }, index) {
   let newTaskLI = document.createElement("LI")
   myTasksUl.appendChild(newTaskLI)
+
   let newTaskRadio = document.createElement("INPUT")
   newTaskRadio.setAttribute("type", "checkbox")
+  // I used 'index' as a way to make a unique id
   newTaskRadio.setAttribute("id", `chechbox${index}`)
   newTaskLI.appendChild(newTaskRadio)
+
   let newTaskLabel = document.createElement("LABEL")
   newTaskLabel.setAttribute("for", `chechbox${index}`)
-  newTaskLabel.innerText = value
+  newTaskLabel.innerText = taskContent
   newTaskRadio.after(newTaskLabel)
-  let line = document.createElement("span")
+
+  let line = document.createElement("SPAN")
   newTaskLabel.appendChild(line)
 
   // Display Task Status : Checked/Unchecked
-  if (checkSt) {
-    // true
+  if (complete) {
     newTaskRadio.setAttribute("checked", "") // Att. without value
-    line.style.width = `${newTaskLabel.offsetWidth}px` // See: https://www.javascripttutorial.net/javascript-dom/javascript-width-height/
+    // Define the cross line's width to be the same as the text's width
+    line.style.width = `${newTaskLabel.offsetWidth}px`
   }
+
   // Add Click Event to Radio Button
   radioEvent(newTaskRadio, line, newTaskLabel)
 }
 
-function radioEvent(newTaskRadio, line, newTaskLabel) {
-  newTaskRadio.addEventListener("click", function (ev) {
-    let lineWidth = `${newTaskLabel.offsetWidth}px` // Get the lebel's width
-    let checkAtt = newTaskRadio.hasAttribute("checked")
-    checkAtt
-      ? changeStatusTo("unchecked", "0")
-      : changeStatusTo("checked", lineWidth)
+function radioEvent(radioElement, line, label) {
+  radioElement.addEventListener("click", function () {
+    try {
+      let lineWidth = `${label.offsetWidth}px` // Get the lebel's width
+      let checkAtt = radioElement.hasAttribute("checked")
+      checkAtt
+        ? changeStatusTo("unchecked", "0")
+        : changeStatusTo("checked", lineWidth)
 
-    function changeStatusTo(status, textWidth) {
-      // Clear Existed attributes
-      newTaskRadio.removeAttribute("checked")
-      newTaskRadio.removeAttribute("unchecked")
+      function changeStatusTo(status, textWidth) {
+        // Clear Existed attributes
+        radioElement.removeAttribute("checked")
+        radioElement.removeAttribute("unchecked")
 
-      newTaskRadio.setAttribute(status, "")
-      line.style.width = textWidth // line width = Label's text width
+        radioElement.setAttribute(status, "")
+        line.style.width = textWidth // line width = Label's text width
 
-      // Save Completed Task's State
-      saveTask(newTaskLabel.innerText, status, ev)
+        // Save Completed Task's State
+        const task_to_save = {
+          taskContent: label.innerText,
+          checked: status,
+        }
+        saveTaskStatus(task_to_save)
+        //! saveTask(task_to_save, ev)
+      }
+    } catch (error) {
+      console.error(error)
     }
+    return false
   })
 }
 
-// Organized function (Temp)
-function saveTask(task, stat, event) {
-  for (var i = 0; i < myLists.length; i++) {
-    if (myLists[i].id == getAttId()) {
-      if (event.type == "submit") {
-        // Add New Task
-        let newTaskObj = { complete: false, taskContent: task }
-        myLists[i].tasks.push(newTaskObj)
-        refrechTasksCounter(myLists[i].tasks)
-        break // leave For Loop
-      }
-
-      if (event.type == "click") {
-        // Radio Button Check
-        stat === "checked" ? (stat = true) : (stat = false)
-        for (let x = 0; x < myLists[i].tasks.length; x++) {
-          let myTask = myLists[i].tasks[x]
-          if (myTask.taskContent == task) {
-            myTask.complete = stat // Update Check Status
-            refrechTasksCounter(myLists[i].tasks)
-            break // leave For Loop
-          }
-        }
-      }
+function saveTaskStatus(task_to_save) {
+  let { taskContent, checked } = task_to_save
+  const list_found = findList(getAttId())
+  if (!list_found) return
+  for (let i = 0; i < list_found.tasks.length; i++) {
+    if (list_found.tasks[i].taskContent == taskContent) {
+      // Update Check Status
+      list_found.tasks[i].complete = checked === "checked" ? true : false
+      break
     }
   }
+  refrechTasksCounter(list_found.tasks)
+  storeUpdated()
+}
+
+function saveTask(task_to_save) {
+  const list_found = findList(getAttId())
+  if (!list_found) return
+  list_found.tasks.push(task_to_save)
+  refrechTasksCounter(list_found.tasks)
   storeUpdated()
 }
 
 tasksForm.addEventListener("submit", function (event) {
   event.preventDefault()
-  if (newTask.value.trim() && getAttId()) {
-    createTask(newTask.value, false, Date.now()) //Date.now() = Random number to add to Input-id & label-for
-    saveTask(newTask.value, false, event)
+  const task_input = newTask.value.trim()
+  if (task_input && getAttId()) {
+    const new_task = { taskContent: task_input, complete: false }
+    //Date.now() = Random number to add to Input-id & label-for
+    createTask(new_task, Date.now())
+    saveTask(new_task)
     newTask.value = ""
   } else {
     alert("Choose a list then write a task.")
@@ -172,13 +185,16 @@ tasksForm.addEventListener("submit", function (event) {
 
 function refrechTasksCounter(tasksArray) {
   let count = 0
-  tasksArray.forEach((el) => {
-    // Reduce?
-    if (!el.complete) count++
-  })
+  if (tasksArray.length > 0) {
+    tasksArray.forEach((el) => {
+      // Reduce?
+      if (!el.complete) count++
+    })
+  }
 
-  if (count < 2) return (tasksCounter.innerText = `${count} task remains.`)
-  tasksCounter.innerText = `${count} tasks remaining.`
+  return (tasksCounter.innerText = `${count} ${
+    count < 2 ? "task" : "tasks"
+  } remains.`)
 }
 
 clearBtn.onclick = function () {
@@ -189,7 +205,7 @@ clearBtn.onclick = function () {
       if (el.id == attId) {
         el.tasks = el.tasks.filter((elem) => elem.complete != true)
         storeUpdated()
-        fillMyTasks(el.id)
+        loadTasks(el.id)
       }
     }
   }
@@ -202,11 +218,11 @@ delBtn.onclick = function (evt) {
     deleteListFromLS(attId)
     storeUpdated()
     clearBefor()
-    fillMyLists(evt) // Displaying Filtred lists
+    loadLists(evt) // Displaying Filtred lists
   }
 }
 function deleteListFromLS(att_id) {
-  myLists = myLists.filter((el) => el.id != att_id) // Or we can use the folowed code:
+  myLists = myLists.filter((list) => list.id != att_id) // Or we can use the folowed code:
 }
 function clearBefor() {
   myTasksUl.replaceChildren("") // Clear Tasks Container
@@ -216,7 +232,9 @@ function clearBefor() {
   myListsUl.textContent = "" // Clear Lists Container
 }
 function getAttId() {
-  return listName.getAttribute("data-list-id")
+  const element_id = listName.getAttribute("data-list-id")
+  // Note: element_id is a String
+  return Number(element_id)
 }
 function storeUpdated() {
   window.localStorage.setItem("Lists.To.Do", JSON.stringify(myLists))
